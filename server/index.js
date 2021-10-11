1f928616d88d05959d758881c2ba6d82190602ad
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-11 16:07:49 +0100

    update votes table

--- server/index.js
+++ server/index.js
@@ -13,35 +13,34 @@ app.use(bodyParser.json());
 app.use(cors());
 let db = new sqlite3.Database("./server/stories.db");
 
-app.get("/api/stories", (req, res) => {
-  const stories = db.all("SELECT * FROM stories", (err, stories) => {
-    res.json({ stories });
-  });
+app.get("/api/stories", async (req, res) => {
+  db.all(
+    `SELECT stories.*,
+    MAX(0, SUM(CASE direction WHEN 'up' THEN 1 ELSE -1 END)) AS score
+    FROM stories
+    LEFT JOIN votes ON votes.story_id = stories.id
+    GROUP BY stories.id;`,
+    (err, stories) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(stories);
+      res.json({ stories });
+    }
+  );
 });
 
 app.post("/api/stories/:id/votes", (req, res) => {
   const { id } = req.params;
   const { direction } = req.body;
 
-  if (direction == "down") {
-    db.run(
-      `
-      UPDATE stories
-      SET score = score-1
-      WHERE
-      id = ?;`,
-      [id]
-    );
-  } else {
-    db.run(
-      `
-      UPDATE stories
-      SET score = score+1
-      WHERE
-      id = ?;`,
-      [id]
-    );
-  }
+  db.run(
+    `
+      INSERT INTO votes(direction,story_id) 
+      VALUES(?,?);
+      `,
+    [direction, id]
+  );
 });
 
 app.post("/api/stories/", (req, res) => {
@@ -57,10 +56,10 @@ app.post("/api/stories/", (req, res) => {
   }
 });
 
-// Have Node serve the files for our built React app
+// Have Node serve the files for built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-// All other GET requests not handled before will return our React app
+// All other GET requests not handled before will return the React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
